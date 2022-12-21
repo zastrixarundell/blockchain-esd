ed1f12587840f594c8ad3c50e41405e134039cac
zastrixarundell <lukabodroza@gmail.com>
2022-12-21 04:45:23 -0800

    Added basic restful smart contract logic

---
+++ SmartContract/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartContract
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+    }
+}
